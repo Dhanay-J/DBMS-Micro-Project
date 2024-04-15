276c2b195d69429c7c0ff1c193f976a43829356b
Dhanay J <jdhanay13@gmail.com>
2024-04-15 23:57:54 +0530

    Added Backend

---
+++ music-app-backend/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const oracledb = require('oracledb');
+const cors = require("cors");
+const port = 3000;
+
+const config = {
+    user: 'system', // User name
+    password: '', // Password for user
+    connectString: '192.168.2.1:1521/xe'
+};
+
+var connection;
+
+async function connect() {
+    try {
+      connection = await oracledb.getConnection(config);
+      console.log('Connected to Oracle Database!');
+    } catch (err) {
+      console.error('Error connecting to Oracle DB:', err);
+    }
+}
+
+
+
+let app = express();
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+connect();  // Connect to Oracle DB
+
+app.get('/data', async (req, res) => {
+    var query = req.query['Query'];
+    console.log(query);
+    try{
+        const result = await connection.execute(query);
+        res.json(result.rows); // Send result as JSON
+    }
+    catch(err){
+        console.log(err);
+    }
+
+});
+
